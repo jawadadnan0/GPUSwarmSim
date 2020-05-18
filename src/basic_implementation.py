5e9a3558c4bbf9f52e2d85b9c33aee4c016ac798
Jawad Adnan <jawadadnan46@yahoo.com>
2020-05-18 17:42:39 +0100

    Completed the code implementation for the algorithm and checked the data output.

--- src/basic_implementation.py
+++ src/basic_implementation.py
@@ -8,13 +8,14 @@ np.set_printoptions(precision=4)
 
 def main():
     n, l, t, r, v, nu, kappa = parse_args()
-#     print(f"""Number of Particles: {n}
-# Periodic Spatial Domain: {l}
-# Total No. of Iterations: {t}
-# Interaction Radius: {r}
-# Initial Particle velocity: {v}
-# Jump Rate: {nu}
-# Concentration Parameter: {kappa}""")
+    # print(f"""Hyperparameters:-
+    #     Number of Particles: {n}
+    #     Periodic Spatial Domain: {l}
+    #     Total No. of Iterations: {t}
+    #     Interaction Radius: {r}
+    #     Initial Particle velocity: {v}
+    #     Jump Rate: {nu}
+    #     Concentration Parameter: {kappa}""")
 
     dt = 0.01 / nu
     max_iter = np.floor(t / dt).astype(int)
@@ -24,14 +25,16 @@ def main():
     vel = 2 * np.pi * np.random.random((n, 1))
     scale = l / 60
 
-#     print(f"""Time Discretisation Step: {dt}
-# Max Iteration: {max_iter}
-# Scaled Velocity of Particles: {scaled_velocity}
-# Scaled Interaction Radius: {rr}
-# Positions of the Particles:
-# {pos}
-# Direction of the Motion of Particles:
-# {vel}""")
+    # print(f"""Calculated Parameters:-
+    #     Time Discretisation Step: {dt}
+    #     Max Iteration: {max_iter}
+    #     Scaled Velocity of Particles: {scaled_velocity}
+    #     Scale: {scale}
+    #     Scaled Interaction Radius: {rr}
+    #     Positions of the Particles:
+    #     {pos}
+    #     Direction of the Motion of Particles:
+    #     {vel}""")
 
     particle_map = np.full((int(l / rr), int(l / rr)), np.nan).astype(np.object)
     index = index_map(pos, rr)
@@ -50,20 +53,57 @@ def main():
         target[np.where(who[:, 0] == 1)] = average_orientation(pos_old, vel_old,
                                                                index_old[np.where(who[:, 0] == 1)],
                                                                particle_map_old, r)
+        vel[np.where(who[:, 0] == 1)] = np.mod(target[np.where(who[:, 0] == 1)] +
+                                               circ_vmrnd(0, kappa, who.sum()), 2 * np.pi)
+        pos = np.mod(pos_old + dt * v * np.c_[np.cos(vel), np.sin(vel)], l)
+
+        index = index_map(pos, rr)
+        particle_map = np.full((int(l / rr), int(l / rr)), np.nan).astype(np.object)
+        particle_map = fill_map(particle_map, index)
+
+        print(pos)
+
+
+def circ_vmrnd(theta=0, kappa=1, n=10):
+    if kappa < 1e-6:
+        return 2 * np.pi * np.random.random((n, 1)) - np.pi
+
+    a = 1 + np.sqrt((1 + 4 * kappa ** 2))
+    b = (a - np.sqrt(2 * a)) / (2 * kappa)
+    r = (1 + b ** 2) / (2 * b)
+
+    alpha = np.zeros((n, 1))
+    for j in range(n):
+        while True:
+            u = np.random.random(size=3)
+
+            z = np.cos(np.pi * u[0])
+            f = (1 + r * z) / (r + z)
+            c = kappa * (r - f)
+
+            if u[1] < c * (2 - c) or not (np.log(c) - np.log(u[1]) + 1 - c < 0):
+                break
+
+        alpha[j, 0] = theta + np.sign(u[2] - 0.5) * np.arccos(f)
+        alpha[j, 0] = np.angle(np.exp(1.0j * alpha[j, 0]))
+
+    return alpha
 
 
 def average_orientation(pos, vel, index, particle_map, r):
-    K = particle_map.shape[0]
+    k = particle_map.shape[0]
     n = index.shape[0]
     ao = np.zeros(shape=(n, 1))
     for i in range(n):
-        I1 = [(index[i, 1] - 1) % K, index[i, 1] % K, (index[i, 1] + 1) % K]
-        I2 = [(index[i, 2] - 1) % K, index[i, 2] % K, (index[i, 2] + 1) % K]
+        first_indexes = [(index[i, 1] - 1) % k, index[i, 1] % k, (index[i, 1] + 1) % k]
+        second_indexes = [(index[i, 2] - 1) % k, index[i, 2] % k, (index[i, 2] + 1) % k]
 
-        neighbours_map = np.asarray([[particle_map[x, y] for x in I1] for y in I2])
+        neighbours_map = np.asarray([[particle_map[x, y] for x in first_indexes] for y in second_indexes])
         neighbours = flatten(neighbours_map)
+
         result = np.linalg.norm(pos[neighbours, :] - pos[index[i, 0], :], ord=2, axis=1, keepdims=True)
         true_neighbours = neighbours[np.where(result < r)[0]]
+
         target = np.sum(np.c_[np.sin(vel[true_neighbours]), np.cos(vel[true_neighbours])], axis=0)
         ao[i, 0] = np.angle(target[0] + 1.0j * target[1])
     return ao
@@ -83,12 +123,10 @@ def flatten(array):
 
 def fill_map(particle_map, index):
     for i in range(len(index)):
-        # print("Before: ", particle_map[index[i, 1], index[i, 2]])
         if np.all(np.isnan(particle_map[index[i, 1], index[i, 2]])):
             particle_map[index[i, 1], index[i, 2]] = [index[i, 0]]
         else:
             particle_map[index[i, 1], index[i, 2]] = np.r_[index[i, 0], particle_map[index[i, 1], index[i, 2]]]
-        # print("After: ", particle_map[index[i, 1], index[i, 2]])
     return particle_map
 
 
