36efa4ebb009f62888f77d08e404d4c36c153002
Jawad Adnan <jawadadnan46@yahoo.com>
2020-07-31 13:34:52 +0100

    pytorch_implementation.py is given program argument option to save file to give file name, added type hints to all functions and increased efficiency in code by not creating particle map after each iteration; just deep-copying an empty version each time.

--- src/pytorch_implementation.py
+++ src/pytorch_implementation.py
@@ -3,8 +3,12 @@ import numpy as np
 import torch
 
 from copy import deepcopy
+from datetime import datetime
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation, writers
+from matplotlib.axes import Axes
+from torch import Tensor
+from typing import Any, Generator, List, Tuple
 
 np.set_printoptions(precision=4)
 if not torch.cuda.is_available():
@@ -14,7 +18,7 @@ gpu_cuda = torch.device("cuda")
 
 
 def main():
-    n, l, t, r, v, nu, kappa = parse_args()
+    save, file, n, l, t, r, v, nu, kappa = parse_args()
     # print(f"""Hyperparameters:-
     #     Number of Particles: {n}
     #     Periodic Spatial Domain: {l}
@@ -23,17 +27,21 @@ def main():
     #     Initial Particle velocity: {v}
     #     Jump Rate: {nu}
     #     Concentration Parameter: {kappa}""")
-
+    current_time = datetime.now()
     fig, ax = plt.subplots(dpi=200)
 
     writer = writers['ffmpeg'](fps=15, metadata=dict(artist="Jawad"), bitrate=1800)
     ani = FuncAnimation(fig, update_quiver_frame, frames=process_particles(n, l, t, r, v, nu, kappa),
                         fargs=(ax, l.item()), interval=30, save_count=int(100 * t.item() * nu.item()) + 1, repeat=False)
-    ani.save("quiver_basic.mp4", writer=writer)
-    # plt.show()
+
+    if save:
+        ani.save(file, writer=writer)
+        print("[100% Complete] Time taken:", (datetime.now() - current_time).seconds, "seconds")
+    else:
+        plt.show()
 
 
-def update_quiver_frame(frame_data, ax, l):
+def update_quiver_frame(frame_data: Tuple[Tensor, Tensor], ax: Axes, l: int) -> None:
     ax.clear()
     sep = l / 10
     ax.set_xticks(np.arange(0, l + sep, sep))
@@ -42,14 +50,16 @@ def update_quiver_frame(frame_data, ax, l):
     ax.set_ylim(0, l)
 
     pos, vel = frame_data
-    scale = tensor(l / 60, torch.float)
+    scale = l / 60
 
     q = ax.quiver(pos[:, 0].tolist(), pos[:, 1].tolist(),
-                  (scale * torch.cos(vel)).tolist(), (scale * torch.sin(vel)).tolist())
+                  torch.mul(torch.cos(vel), scale).flatten().tolist(),
+                  torch.mul(torch.sin(vel), scale).flatten().tolist())
     ax.quiverkey(q, X=0.3, Y=1.1, U=0.05, label='Quiver key, length = 0.05', labelpos='E')
 
 
-def process_particles(n, l, t, r, v, nu, kappa):
+def process_particles(n: int, l: Tensor, t: Tensor, r: Tensor, v: Tensor, nu: Tensor, kappa: Tensor) -> \
+        Generator[Tuple[Tensor, Tensor], None, None]:
     dt = tensor(0.01, torch.float) / nu
     max_iter = torch.floor(t / dt).to(torch.int64).item() * 5
     scaled_velocity = l * v
@@ -80,11 +90,13 @@ def process_particles(n, l, t, r, v, nu, kappa):
         target[torch.where(torch.eq(who[:, 0], 1))] = \
             average_orientation(pos, target, index[torch.where(torch.eq(who[:, 0], 1))], particle_map, r)
         vel[torch.where(torch.eq(who[:, 0], 1))] = \
-            torch.remainder(target[torch.where(torch.eq(who[:, 0], 1))] + von_mises_dist(0, kappa, torch.sum(who).item()),
+            torch.remainder(target[torch.where(torch.eq(who[:, 0], 1))] +
+                                von_mises_dist(tensor(0, torch.float), kappa, torch.sum(who).item()),
                             tensor(2 * np.pi, torch.float))
         pos = torch.remainder(pos + dt * scaled_velocity * torch.cat((torch.cos(vel), torch.sin(vel)), 1), l)
 
         if t % 10 == 0:
+            print(f"Iteration number: {t} (out of {max_iter} iterations) [{(100 * t) // max_iter}% complete]")
             yield pos, vel
 
         index = index_map(pos, rr)
@@ -92,7 +104,7 @@ def process_particles(n, l, t, r, v, nu, kappa):
         particle_map = fill_map(particle_map, index)
 
 
-def von_mises_dist(theta, kappa, n):
+def von_mises_dist(theta: Tensor, kappa: Tensor, n: int) -> Tensor:
     pi = tensor(np.pi, torch.float)
     tensors = [tensor(num, torch.int64) for num in range(5)]
 
@@ -122,7 +134,8 @@ def von_mises_dist(theta, kappa, n):
     return alpha
 
 
-def average_orientation(pos, vel, index, particle_map, r):
+def average_orientation(pos: Tensor, vel: Tensor, index: Tensor,
+                        particle_map: np.ndarray, r: Tensor) -> Tensor:
     k = particle_map.shape[0]
     n = index.size()[0]
     ao = torch.zeros(n, 1, device=gpu_cuda)
@@ -139,18 +152,18 @@ def average_orientation(pos, vel, index, particle_map, r):
     return ao
 
 
-def flatten(array):
+def flatten(array_matrix: List[List[Any]]) -> Tensor:
     result = []
-    for x in range(len(array)):
-        for y in range(len(array[0])):
+    for x in range(len(array_matrix)):
+        for y in range(len(array_matrix[0])):
             try:
-                result += list(array[x][y])
+                result += list(array_matrix[x][y])
             except TypeError:
-                result += [array[x][y]]
+                result += [array_matrix[x][y]]
     return tensor([e for e in result if not np.isnan(e)], torch.int64)
 
 
-def fill_map(particle_map, index):
+def fill_map(particle_map: np.ndarray, index: Tensor) -> np.ndarray:
     for i in range(index.size()[0]):
         if np.all(np.isnan(particle_map[index[i, 1], index[i, 2]])):
             particle_map[index[i, 1], index[i, 2]] = [index[i, 0].item()]
@@ -159,20 +172,23 @@ def fill_map(particle_map, index):
     return particle_map
 
 
-def index_map(pos, r):
+def index_map(pos: Tensor, r: Tensor) -> Tensor:
     indexes = torch.arange(pos.size()[0], device=gpu_cuda).reshape(pos.size()[0], 1)
     return torch.cat((indexes, torch.floor(pos / r).to(torch.int64)), 1)
 
 
-def tensor(value, data_type):
+def tensor(value: Any, data_type: Any) -> Tensor:
     return torch.tensor(value, dtype=data_type, device=gpu_cuda)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Depicting the movement of several quaternions in a 3D space")
+def parse_args() -> Tuple[bool, str, int, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+    parser = argparse.ArgumentParser(description="Depicting the movement of several particles in a 2D "
+                                                 "space using only the GPU.")
 
-    parser.add_argument("-n", "--agents_num", type=int, default=1000000, help="The Number of Agents")
-    parser.add_argument("-l", "--box_size", type=int, default=100, help="The Size of the Box (Periodic Spatial Domain)")
+    parser.add_argument("-s", "--save", action="store_true", default=False, help="Save in a File or not.")
+    parser.add_argument("-f", "--video_file", type=str, default="quiver_pytorch.mp4", help="The Video File to Save in")
+    parser.add_argument("-n", "--agents_num", type=int, default=5000, help="The Number of Agents")
+    parser.add_argument("-l", "--box_size", type=int, default=1, help="The Size of the Box (Periodic Spatial Domain)")
     parser.add_argument("-t", "--max_iter", type=int, default=50, help="The Total Number of Iterations/Seconds")
     parser.add_argument("-r", "--interact_radius", type=float, default=0.07, help="The Radius of Interaction")
     parser.add_argument("-v", "--particle_velocity", type=float, default=0.02, help="The Velocity of the Particles")
@@ -181,10 +197,10 @@ def parse_args():
 
     args = parser.parse_args()
 
-    return tensor(args.agents_num, torch.int64), tensor(args.box_size, torch.int64), \
-           tensor(args.max_iter, torch.int64), tensor(args.interact_radius, torch.float), \
-           tensor(args.particle_velocity, torch.float), tensor(args.jump_rate, torch.float), \
-           tensor(args.concentration, torch.float)
+    return args.save, args.video_file, args.agents_num, \
+           tensor(args.box_size, torch.int64), tensor(args.max_iter, torch.int64), \
+           tensor(args.interact_radius, torch.float), tensor(args.particle_velocity, torch.float), \
+           tensor(args.jump_rate, torch.float), tensor(args.concentration, torch.float)
 
 
 if __name__ == '__main__':
